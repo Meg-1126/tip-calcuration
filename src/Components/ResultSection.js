58a81247cb69d86aeedc038ad61e333fe2fab3e0
Megumi Akama <kamayatu91@gmail.com>
2022-08-17 11:14:20 -0700

    Add tip calculation event

--- src/Components/ResultSection.js
+++ src/Components/ResultSection.js
@@ -1,13 +1,23 @@
 import React from "react";
 
-export default function ResultSection () {
+export default function ResultSection (props) {
+  const {bill, setBill, tip, setTip, total, setTotal} = props;
+
+  const calculateTip = () => {
+    setTip(bill * 0.05);
+  }
+
+  const handleClick = () => {
+    alert("Reset button is clicked"); //change this later
+  }
+
   return (
     <section>
       <p>Tip Amount<br/>/ person</p>
-      <p>$</p>
+      <p>${tip}</p>
       <p>Total<br/>/ person</p>
-      <p>$</p>
-      <button className="btn__reset">RESET</button>
+      <p>${total}</p>
+      <button className="btn__reset" onClick={handleClick}>RESET</button>
     </section>
   );
 }
